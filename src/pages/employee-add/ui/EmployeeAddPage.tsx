7a15c9966d84759ab4a3eb010cd0bbc0b5464be1
Ildar <ildar.1795@gmail.com>
2025-02-16 19:15:29 +0400

    feat: Создает страницы для списка сотрудников, редактирования и добавления новых

---
+++ src/pages/employee-add/ui/EmployeeAddPage.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import { AddEmployee } from '../../../features/add-employee'
+
+interface EmployeeAddPageProps {}
+
+export const EmployeeAddPage: FC<EmployeeAddPageProps> = () => {
+	return <AddEmployee />
+}
---
+++ src/pages/employee-edit-page/ui/EmployeeEditPage.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import { EditEmployee } from '../../../features/edit-employee'
+
+interface EmployeeEditPageProps {}
+
+export const EmployeeEditPage: FC<EmployeeEditPageProps> = ({}) => {
+	return <EditEmployee />
+}
